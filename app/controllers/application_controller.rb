edad4a261aa81adcbff3bafd962a84e7bfa9323e
Ryan Mullen <ryanfmullen@gmail.com>
2022-09-02 17:19:52 -0700

    fix bug protect_from_forgery module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+    include ActionController::RequestForgeryProtection
 
     protect_from_forgery with: :exception
     before_action :snake_case_params
