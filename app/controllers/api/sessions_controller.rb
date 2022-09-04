d5598f01957fb129e956bd371170f17dc434dba7
Ryan Mullen <ryanfmullen@gmail.com>
2022-09-04 14:00:30 -0700

    build routes, user and session controller, and user show jbuilder

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+  def show
+    if current_user
+      @user = current_user
+      render 'api/users/show'
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:email], params[:password])
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: { errors: ['The provided credentials were invalid.'] }, 
+        status: :unauthorized
+    end
+  end
+
+  def destroy
+    logout!
+    render json: { message: 'successfully logged out' }
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,10 +25,14 @@ ApplicationRecord.transaction do
 
     # More users
     10.times do 
+        first_name = Faker::Name.first_name
+        last_name = Faker::Name.last_name
+        email = Faker::Internet.unique.email(name: first_name + ' ' + last_name)
+
     User.create!({
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        email: Faker::Internet.unique.email,
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
         password: 'password'
     }) 
     end
