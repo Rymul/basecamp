58cbcf1ca0da3e41a6c4d54959681471fc7d5d13
Ryan Mullen <ryanfmullen@gmail.com>
2022-09-04 13:00:24 -0700

    create seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    # Unnecessary if using `rails db:seed:replant`
+    User.destroy_all
+  
+    puts "Resetting primary keys..."
+    # For easy testing, so that after seeding, the first `User` has `id` of 1
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+  
+    puts "Creating users..."
+
+    User.create!(
+        first_name: 'Demo',
+        last_name: 'User',
+        email: 'demo@user.com', 
+        password: 'password'
+    )
+
+    # More users
+    10.times do 
+    User.create!({
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.unique.email,
+        password: 'password'
+    }) 
+    end
+end
