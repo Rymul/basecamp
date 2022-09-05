d723e797cda1ed518b16b4b4c7d3b8847fc284fd
Ryan Mullen <ryanfmullen@gmail.com>
2022-09-05 09:42:13 -0700

    build csrf protection for frontend

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # test route for application_controller
-  post 'api/test', to: 'application#test'
-
-
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
     window.store = store;
-    // window.csrfFetch = csrfFetch;
+    window.csrfFetch = csrfFetch;
     // window.sessionActions = sessionActions;
 }
 
@@ -23,10 +24,17 @@ function Root() {
     </Provider>
   )
 }
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
+}
