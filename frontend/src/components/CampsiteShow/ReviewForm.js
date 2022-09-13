b578ed4ee7e342e9da236c7ddc301e7d1f5d2692
Ryan Mullen <ryanfmullen@gmail.com>
2022-09-13 12:31:30 -0700

    add review form styling

--- frontend/src/components/CampsiteShow/ReviewForm.js
+++ frontend/src/components/CampsiteShow/ReviewForm.js
@@ -46,6 +46,9 @@ const ReviewForm = () => {
 
     return(
         <div className='reviewForm-component'>
+            {/* <ul className="errors">
+                {errors.map(error => <li className="error" key={error}>{error}</li>)}
+            </ul> */}
             <div className='title'>
                 <h1>Thank you for your stay at {`${campsite.name}`}.</h1>
                 <p>Your feedback is truly appreciated to make future camping better for all!</p>
@@ -53,22 +56,36 @@ const ReviewForm = () => {
             {/* {!sessionUser && <ReviewModal onClose={closeForm} />} */}
             <div className='review-form-container'>
                 <form onSubmit={handleSubmit} className='review-form'>
-                    <label id='title'>Title: 
-                        <input id='title-input' type='text' value={review.title} onChange={handleChange("title")} />
-                    </label>
-                    <label id='body'>Body:
-                        <textarea id='body-input' maxLength="200" cols="50" rows="4" value={review.body} onChange={handleChange("body")}></textarea>
-                    </label>
-                    <label id='rating'>Rating</label>
-                        <input type="radio" name="rating" id="rating-input" value='1' onChange={handleChange}/>1 
-                        <input type="radio" name="rating" id="rating-input" value='2' onChange={handleChange}/>2 
-                        <input type="radio" name="rating" id="rating-input" value='3' onChange={handleChange}/>3 
-                        <input type="radio" name="rating" id="rating-input" value='4' onChange={handleChange}/>4 
-                        <input type="radio" name="rating" id="rating-input" value='5' onChange={handleChange}/>5 
+                    {/* <label id='title'>Title:  */}
+                        <input 
+                            id='review-form-title'
+                            placeholder='Title...' 
+                            type='text' 
+                            value={review.title} 
+                            onChange={handleChange("title")} 
+                        />
+                    {/* </label> */}
+                    {/* <label id='body'>Body: */}
+                        <textarea 
+                            id='review-form-body' 
+                            placeholder='Body...' 
+                            maxLength="200" cols="50" rows="4" 
+                            value={review.body} 
+                            onChange={handleChange("body")}>
+                        </textarea>
+                    {/* </label> */}
+                    <div className='rating'>
+                        <label id='rating'>Rating:</label>
+                            <input type="radio" name="rating" id="rating-input" value='1' onChange={handleChange}/>1 
+                            <input type="radio" name="rating" id="rating-input" value='2' onChange={handleChange}/>2 
+                            <input type="radio" name="rating" id="rating-input" value='3' onChange={handleChange}/>3 
+                            <input type="radio" name="rating" id="rating-input" value='4' onChange={handleChange}/>4 
+                            <input type="radio" name="rating" id="rating-input" value='5' onChange={handleChange}/>5 
+                    </div>
                     <label id='recommended'>Recommended
                         <input id='recommended-input' type="checkbox" value={review.recomended} onChange={handleChange} />
                     </label>
-                    <button>Create Review</button>
+                    <button id='submit-button'>Create Review</button>
                 </form>
             </div>
         </div>
